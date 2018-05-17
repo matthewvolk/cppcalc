4060356a62ac118416803108eb1e5f148a6aaaf2
matthewvolk <volkmattj@gmail.com>
2018-05-17 08:21:02 -0700

    create vscode tasks.json and configure build for g++

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,37 +5,40 @@ using namespace std;
 class Calculator
 {
 public:
-  int Calculate (int, int, char);
+  int Calculate(int, int, char);
 }
 
 int main()
 {
   int x, y, result;
   char operatr;
-  cout<< "I'm a calculator" <<endl;
-  cout<< "Please enter num1, operator, num2: " <<endl;
-
+  cout << "I'm a calculator" << endl;
+  cout << "Please enter num1, operator, num2: " << endl;
+  cin >> x >> y >> operatr;
+  Calculator c;
+  result = c.Calculate(x, y, operatr);
+  cout << "Result is: " << result << endl;
 
   cin.ignore();
   cin.get();
   return 0;
 }
 
-int Calculator::Calculate (int x, int y, char operatr)
+int Calculator::Calculate(int x, int y, char operatr)
 {
   switch (operatr)
   {
-    case '+':
-      return x + y;
-    case '-':
-      return x - y;
-    case '*':
-      return x * y;
-    case '/':
-      if (y != 0)
-        return x / y;
-    default:
-      cin >> x >> operatr >> y;
-      Calculate(x, y, operatr);
+  case '+':
+    return x + y;
+  case '-':
+    return x - y;
+  case '*':
+    return x * y;
+  case '/':
+    if (y != 0)
+      return x / y;
+  default:
+    cin >> x >> operatr >> y;
+    Calculate(x, y, operatr);
   }
 }
