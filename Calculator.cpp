5b2b9417fa8454b90e4cdf184dbe6781565d775e
matthewvolk <volkmattj@gmail.com>
2018-05-16 22:58:19 -0700

    first commit

---
+++ Calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator
+{
+public:
+  int Calculate (int, int, char);
+}
+
+int main()
+{
+  std::cout<< "Hello! I'm a calculator" <<endl;
+
+  cin.ignore();
+  cin.get();
+  return 0;
+}
